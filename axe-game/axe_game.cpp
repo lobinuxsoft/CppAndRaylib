4a9cd7aa454b013ffd590cb2e1c517f30c0e8e72
Matias Galarza <lobinuxsoft@gmail.com>
2024-01-26 20:23:21 -0300

    Draw text

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -12,9 +12,25 @@ int main()
     float circle_radius{25.0f};
     float circle_speed{10};
 
+    // circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
     // Axe variables
     int axe_x{400};
     int axe_y{0};
+    int axe_lenght{50};
+    int direction{10};
+
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_lenght};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_x + axe_lenght};
+
+    bool collision_with_axe{true};
 
     InitWindow(width, height, title);
 
@@ -24,26 +40,36 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // Game logic begins
+        if (collision_with_axe)
+        {
+            DrawText("Game Over!", 400, 200, 20, RED);
+        }
+        else
+        {
+            // Game logic begins
 
-        if (IsKeyDown(KEY_D) && (circle_x + circle_radius) < width)
-            circle_x += circle_speed;
+            if (IsKeyDown(KEY_D) && (circle_x + circle_radius) < width)
+                circle_x += circle_speed;
 
-        if (IsKeyDown(KEY_A) && (circle_x - circle_radius) > 0)
-            circle_x -= circle_speed;
+            if (IsKeyDown(KEY_A) && (circle_x - circle_radius) > 0)
+                circle_x -= circle_speed;
 
-        if (IsKeyDown(KEY_W) && (circle_y - circle_radius) > 0)
-            circle_y -= circle_speed;
+            if (IsKeyDown(KEY_W) && (circle_y - circle_radius) > 0)
+                circle_y -= circle_speed;
 
-        if (IsKeyDown(KEY_S) && (circle_y + circle_radius) < height)
-            circle_y += circle_speed;
+            if (IsKeyDown(KEY_S) && (circle_y + circle_radius) < height)
+                circle_y += circle_speed;
 
-        axe_y += 10;
+            axe_y += direction;
 
-        // Draw on screen
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            if (axe_y > height || axe_y < 0)
+                direction = -direction;
 
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+            // Draw on screen
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+
+            DrawRectangle(axe_x, axe_y, axe_lenght, axe_lenght, RED);
+        }
 
         // Game logic ends
         EndDrawing();
