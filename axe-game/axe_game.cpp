5529d5dcd79050f7537b6ebf8f973d8e3717db44
Matias Galarza <lobinuxsoft@gmail.com>
2024-01-26 19:07:18 -0300

    Circle movement

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -2,17 +2,40 @@
 
 int main()
 {
-    int width = 920;
-    int height = 640;
-    const char *title = "Axe Game";
+    int width{920};
+    int height{640};
+    const char *title{"Axe Game"};
+
+    int circle_x{width / 2};
+    int circle_y{height / 2};
+    float circle_radius{25.0f};
+    float circle_speed{10};
 
     InitWindow(width, height, title);
 
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawCircle(width / 2, height / 2, 25.0f, BLUE);
+
+        // Game logic begins
+
+        if (IsKeyDown(KEY_D) && (circle_x + circle_radius) < width)
+            circle_x += circle_speed;
+
+        if (IsKeyDown(KEY_A) && (circle_x - circle_radius) > 0)
+            circle_x -= circle_speed;
+
+        if (IsKeyDown(KEY_W) && (circle_y - circle_radius) > 0)
+            circle_y -= circle_speed;
+
+        if (IsKeyDown(KEY_S) && (circle_y + circle_radius) < height)
+            circle_y += circle_speed;
+
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+
+        // Game logic ends
         EndDrawing();
     }
 }
