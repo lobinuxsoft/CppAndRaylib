0b9a3063247f1e5cb986bb7a61901c898ecd52e6
Matias Galarza <lobinuxsoft@gmail.com>
2024-02-01 17:50:27 -0300

    Drawing the character

--- classy-clash/main.cpp
+++ classy-clash/main.cpp
@@ -12,6 +12,9 @@ int main()
     Vector2 mapPos{0.0, 0.0};
     float speed{4.0};
 
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{windowWith / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f), (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -21,18 +24,26 @@ int main()
 
         Vector2 direction{};
 
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0;
 
-        if(Vector2Length(direction) != 0.0)
+        if (Vector2Length(direction) != 0.0)
         {
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
-        
 
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        Rectangle source{0.f, 0.f, (float)knight.width / 6, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
         EndDrawing();
     }
